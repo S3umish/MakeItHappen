e5bf4b6b4bb1928b3a0accae952c016144f6d889
S1umish <sunitee.mishra@gmail.com>
2020-12-24 14:24:20 -0500

    updated helper method -authorized to edit  and views error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,10 +26,10 @@ class ApplicationController < Sinatra::Base
       @current_user ||=User.find_by(id:session[:user_id])
     end
 
+    def authorized_to_edit?(mission)
+      mission.user == current_user
+    end
 
-
-
-    
   end
 
 
