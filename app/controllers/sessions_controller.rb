083dcdeb7d73bcf9e49bbdab4ebbb05d1fbb5c3a
S1umish <sunitee.mishra@gmail.com>
2020-12-23 20:35:32 -0500

    created signup and logout with functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,26 @@ class SessionsController < ApplicationController
         erb :login
     end
 
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash[:message] = "Welcome!!"
+            redirect '/missions'
+        else
+            flash[:error] = "Either username or password is incorrect, please try again."
+            redirect '/login'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.clear
+            redirect '/'
+        else
+            redirect '/'
+        end
+    end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+    get '/signup' do
+        erb :'/users/signup'
+    end
 
-    
+    post '/signup' do
+        user = User.new(params)
+        if user.save
+            session[:user_id]= user.id
+            redirect "/missions"
+        else
+            @errors = user.errors.full_messages.to_sentence
+            erb :"/users/signup"
+        end
+    end
+
 end
