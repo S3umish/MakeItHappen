b5bb916345b465c960746f6143fc994683592b8b
S1umish <sunitee.mishra@gmail.com>
2020-12-24 12:48:40 -0500

    updated mission views files and welcome , working on controller actions

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -4,6 +4,14 @@ class MissionsController < ApplicationController
         @missions = Mission.all 
         erb :"/missions/index"
     end
+
+    get "/missions/new" do
+        if logged_in?
+            erb :"/missions/new"
+        else
+            redirect "/"
+        end
+    end
     
 
 end
