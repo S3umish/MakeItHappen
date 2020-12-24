4f5c518531a16e4aa4ebbbb81e201f0c9bde10db
S1umish <sunitee.mishra@gmail.com>
2020-12-23 20:27:15 -0500

    created login view ,updated Gemfile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
+    get '/login' do
+        if logged_in?
+            redirect"/missions"
+        end
+        erb :login
+    end
 
-    
+
 end
